a19e8c2ca1d9bbb2bc15e8e27e737d109ca83128
Tasha McGlashan <tmcglashan77@gmail.com>
2025-04-21 15:44:42 -0700

    updated App.js

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import Nav from './components/Nav'
-import Landing from './components/Landing'
-import Highlights from'./components/Highlights'
 
+import Nav from './components/Nav'
+import Home from './pages/Home';
+import Footer from './components/Footer'
+import Books from './pages/Books';
+import {BrowserRouter as Router,Route } from 'react-router-dom'
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" component={Books} />
+      <Footer />
+      
     </div>
+
+    </Router>
   );
 }
 
